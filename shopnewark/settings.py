1999da7a4cf48ef4842ab2605fe2dedd65b343e9
jose-dom <josedom000@gmail.com>
2020-03-24 00:47:27 -0400

    setting environment vars

--- shopnewark/settings.py
+++ shopnewark/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&3aczi&^^!)ne@sjgkbl9jt4yo*3p9z2bys87(j1v)2j_(ydks'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
