8b3ec858ed2390184be17ba9a313bc48f522fc71
jose-dom <josedom000@gmail.com>
2020-04-10 00:21:23 -0400

    update

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from users import views as user_views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.home, name='core-home'),
     path('about/', views.about, name='about'),
